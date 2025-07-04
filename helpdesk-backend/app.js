8219a9dd458d4dd08d3a26c187815d577805caf1
Standard Box <ashmit2704@gmail.com>
2025-07-04 18:15:34 +0530

    some minor changes

--- helpdesk-backend/app.js
+++ helpdesk-backend/app.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
 app.use('/auth', authRouter);
 
 const PORT = process.env.PORT || 3000;
